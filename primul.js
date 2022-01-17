d7bee768256541049458851fd8a67284ed905810
Cernolev Corina <corina.cernolev@gmail.com>
2022-01-17 20:02:54 +0300

    proiect

---
+++ primul.js
@@ -0,0 +1,29 @@
+function myFunction() {
+    document.getElementById("java").style.display = "none";
+    document.getElementById("cont").style.display = "grid";
+}
+
+function myFunction1() {
+    document.getElementById("cont").style.display = "none";
+    document.getElementById("java").style.display = "grid";
+}
+
+function myFunction2() {
+    document.getElementById("cont").style.display = "none";
+    document.getElementById("cont1").style.display = "grid";
+}
+
+function myFunction3() {
+    document.getElementById("cont1").style.display = "none";
+    document.getElementById("cont").style.display = "grid";
+}
+
+function myFunction4() {
+    document.getElementById("cont1").style.display = "none";
+    document.getElementById("cont2").style.display = "grid";
+}
+
+function myFunction5() {
+    document.getElementById("cont2").style.display = "none";
+    document.getElementById("cont1").style.display = "grid";
+}
